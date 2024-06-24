afb84b14102e8f448d0b446196cd9c954e637a63
Dmitrii <dmitrii.bychenko@soti.net>
2024-06-24 10:39:50 +0100

    Timer Event

--- Simple.TimeMachine.Tests/TimeMachineTimeProviderExtensionsTest.cs
+++ Simple.TimeMachine.Tests/TimeMachineTimeProviderExtensionsTest.cs
@@ -1,5 +1,71 @@
 ﻿namespace Simple.TimeMachine.Tests;
 
-public sealed class TimeMachineTimeProviderExtensionsTest {
-}
+public sealed class TimeMachineTimeProviderExtensionsTest
+{
+    [Fact]
+    public void Adjust_Spans_TimersAreCalled()
+    {
+        // Arrange
+        var provider = new TimeMachineTimeProvider(new DateTime(2000, 1, 1));
 
+        var calls = new List<string?>();
+
+        provider.CreateTimer(s => calls.Add(s?.ToString()), "A", TimeSpan.Zero, TimeSpan.FromSeconds(1));
+        provider.CreateTimer(s => calls.Add(s?.ToString()), "B", TimeSpan.Zero, TimeSpan.FromSeconds(2));
+        provider.CreateTimer(s => calls.Add(s?.ToString()), "C", TimeSpan.Zero, TimeSpan.FromSeconds(3));
+
+        // Act
+        provider.Adjust(new[] { TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(3), TimeSpan.FromSeconds(7) });
+
+        // Assert
+        string sequence = string.Concat(calls);
+
+        Assert.Equal("ABCAABACABAABCAABACABA", sequence);
+    }
+
+    [Fact]
+    public void Adjust_TimeOffsets_TimersAreCalled()
+    {
+        // Arrange
+        var at = new DateTime(2000, 1, 1);
+
+        var provider = new TimeMachineTimeProvider(at);
+
+        var calls = new List<string?>();
+
+        provider.CreateTimer(s => calls.Add(s?.ToString()), "A", TimeSpan.Zero, TimeSpan.FromSeconds(1));
+        provider.CreateTimer(s => calls.Add(s?.ToString()), "B", TimeSpan.Zero, TimeSpan.FromSeconds(2));
+        provider.CreateTimer(s => calls.Add(s?.ToString()), "C", TimeSpan.Zero, TimeSpan.FromSeconds(3));
+
+        // Act
+        provider.Adjust(new[] { at.AddSeconds(1), at.AddSeconds(4), at.AddSeconds(11) });
+
+        // Assert
+        string sequence = string.Concat(calls);
+
+        Assert.Equal("ABCAABACABAABCAABACABA", sequence);
+    }
+
+    [Fact]
+    public void Adjust_Times_TimersAreCalled()
+    {
+        // Arrange
+        var at = new DateTime(2000, 1, 1);
+
+        var provider = new TimeMachineTimeProvider(at);
+
+        var calls = new List<string?>();
+
+        provider.CreateTimer(s => calls.Add(s?.ToString()), "A", TimeSpan.Zero, TimeSpan.FromSeconds(1));
+        provider.CreateTimer(s => calls.Add(s?.ToString()), "B", TimeSpan.Zero, TimeSpan.FromSeconds(2));
+        provider.CreateTimer(s => calls.Add(s?.ToString()), "C", TimeSpan.Zero, TimeSpan.FromSeconds(3));
+
+        // Act
+        provider.Adjust(new[] { at.AddSeconds(1), at.AddSeconds(4), at.AddSeconds(11) });
+
+        // Assert
+        string sequence = string.Concat(calls);
+
+        Assert.Equal("ABCAABACABAABCAABACABA", sequence);
+    }
+}
--- Simple.TimeMachine/TimeMachineTimeProviderExtensions.cs
+++ Simple.TimeMachine/TimeMachineTimeProviderExtensions.cs
@@ -3,30 +3,47 @@
 /// <summary>
 /// TimeMachine Time Provider extensions
 /// </summary>
-public static class TimeMachineTimeProviderExtensions {
-  /// <summary>
-  /// Move Several Steps
-  /// </summary>
-  /// <param name="provider">Time Provider</param>
-  /// <param name="steps">Steps</param>
-  public static void Move(this TimeMachineTimeProvider provider, IEnumerable<TimeSpan> steps) {
-    ArgumentNullException.ThrowIfNull(provider);
-    ArgumentNullException.ThrowIfNull(steps);
+public static class TimeMachineTimeProviderExtensions
+{
+    /// <summary>
+    /// Move Several Steps
+    /// </summary>
+    /// <param name="provider">Time Provider</param>
+    /// <param name="steps">Steps</param>
+    public static void Adjust(this TimeMachineTimeProvider provider, IEnumerable<TimeSpan> steps)
+    {
+        ArgumentNullException.ThrowIfNull(provider);
+        ArgumentNullException.ThrowIfNull(steps);
 
-    foreach (var step in steps)
-      provider.Move(step);
-  }
+        foreach (var step in steps)
+            provider.Move(step);
+    }
 
-  /// <summary>
-  /// Move Several Steps
-  /// </summary>
-  /// <param name="provider">Time Provider</param>
-  /// <param name="exactTimes">Exact Time Moments to moves</param>
-  public static void Move(this TimeMachineTimeProvider provider, IEnumerable<DateTimeOffset> exactTimes) {
-    ArgumentNullException.ThrowIfNull(provider);
-    ArgumentNullException.ThrowIfNull(exactTimes);
+    /// <summary>
+    /// Move Several Steps
+    /// </summary>
+    /// <param name="provider">Time Provider</param>
+    /// <param name="exactTimes">Exact Time Moments to moves</param>
+    public static void Adjust(this TimeMachineTimeProvider provider, IEnumerable<DateTimeOffset> exactTimes)
+    {
+        ArgumentNullException.ThrowIfNull(provider);
+        ArgumentNullException.ThrowIfNull(exactTimes);
 
-    foreach (var step in exactTimes.OrderBy(item => item))
-      provider.Move(step);
-  }
+        foreach (var step in exactTimes.OrderBy(item => item))
+            provider.Move(step);
+    }
+
+    /// <summary>
+    /// Move Several Steps
+    /// </summary>
+    /// <param name="provider">Time Provider</param>
+    /// <param name="exactTimes">Exact Time Moments to moves</param>
+    public static void Adjust(this TimeMachineTimeProvider provider, IEnumerable<DateTime> exactTimes)
+    {
+        ArgumentNullException.ThrowIfNull(provider);
+        ArgumentNullException.ThrowIfNull(exactTimes);
+
+        foreach (var step in exactTimes.OrderBy(item => item))
+            provider.Move(step);
+    }
 }
